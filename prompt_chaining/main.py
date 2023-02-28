0252bbd2d6123623875cdbc405be41e8c0715d37
Shrey Joshi <shreyjoshi2004@gmail.com>
2023-02-28 17:40:21 -0600

    add stuff

--- prompt_chaining/main.py
+++ prompt_chaining/main.py
@@ -3,29 +3,41 @@ import os
 import dotenv
 import json
 
-dotenv.load_dotenv("../.env")
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
-print(openai.api_key)
+def load_openai_key():
+    dotenv.load_dotenv("../.env")
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+    print(openai.api_key)
 
 
-# load the prompt
-with open("./prompts/comp.txt", "r") as f:
-    comp_prompt = f.read()
+def load_prompt(path):
+    with open(path, "r") as f:
+        comp_prompt = f.read()
+    return comp_prompt
 
-# # run comp prompt
-# response = openai.Completion.create(
-#     engine="text-davinci-003",
-#     prompt=comp_prompt,
-#     temperature=0,
-#     max_tokens=500,
-#     top_p=1,
-#     frequency_penalty=0,
-#     presence_penalty=0,
-# )
 
-# # export to json
-# print(response.choices[0].text, file=open("./comp_out.json", "w+"))
+def run_prompt(prompt, max_tokens=1000):
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        temperature=0,
+        max_tokens=max_tokens,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0,
+    )
+    return response.choices[0].text
+
+
+def save_json(json_str, path):
+    with open(path, "w") as f:
+        f.write(json_str)
+
+
+def load_json(path):
+    with open(path, "r") as f:
+        json_str = f.read()
+    return json_str
 
 
 # build tree from json
@@ -41,11 +53,20 @@ def build_tree(components, level=0, tree={"tree": ""}):
     return tree["tree"]
 
 
-# load json_str from the jsonfile
-with open("./comp_out.json", "r") as f:
-    json_str = f.read()
-
 # Example usage with the given JSON object
 components = json.loads(json_str)
 tree = build_tree(components)
 print(tree)
+
+
+if __name__ == "__main__":
+    load_openai_key()
+
+    # run prompt
+    prompt = load_prompt("./prompts/idea.txt")
+    idea_response = run_prompt(prompt)
+    save_json(response, "idea.json")
+    json_str = load_json("idea.json")
+    components = json.loads(json_str)
+    tree = build_tree(components)
+    print(tree)
