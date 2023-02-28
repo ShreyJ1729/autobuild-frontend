f9bb6dbd6ca180f10344f94a29c4c27388c94199
Shrey Joshi <shreyjoshi2004@gmail.com>
2023-02-27 23:27:37 -0600

    made into monorepo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
-import logo from "./logo.svg";
-import "./App.css";
-import Chatbox from "./chatbox";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
@@ -19,7 +18,6 @@ function App() {
           Learn React
         </a>
       </header>
-      <Chatbox />
     </div>
   );
 }
